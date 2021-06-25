06cb01f2757d7b68bedf12d8eb0fbfe538a75d06
main1479 <m.main2402@gmail.com>
2021-06-25 15:34:10 +0600

    CMS added

--- .eleventy.js
+++ .eleventy.js
@@ -5,5 +5,6 @@ module.exports = function (eleventyConfig) {
 	});
 	eleventyConfig.addPassthroughCopy('./src/style.css');
 	eleventyConfig.addPassthroughCopy('./src/assets');
+	eleventyConfig.addPassthroughCopy('./src/admin');
 	return { dir: { input: './src', output: './public' } };
 };
